cc025e0b993f11ec2a6077faa12da0acb15f0263
Michael Murphy <michaelhmurphy99@gmail.com>
2022-07-09 16:16:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, giftType) {
+    let cards = []
+    for (let i = 0;i < names.length;i++ ) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)
+    }
+    return cards
+}
+function countDown(numOfIterations) {
+    let countDown = numOfIterations
+    while (countDown >=0) {
+      console.log(countDown)
+      countDown--
+    }
+}
